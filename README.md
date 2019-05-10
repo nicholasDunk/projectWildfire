c370be0de782783b705b57aadc94f919ea1a1cc4
Craig Dunk <craig@goproperly.com>
2019-05-09 21:29:32 -0400

    add phaser and a basic hello world application

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 A project of Dunk Development Ltd.
 In Partnership with Fire(tm)
 featuring hexes! Wildfires! Demigods and fire mountains!
+
+## Dev Environment
+
+This project is configured to use webpack and babel so that es6 javascript features can be used
+
+Additionally the Phaser game library is installed to provide tools for presenting game UI
+
+
+## Installing
+
+From the project directory 
+run `npm install` this installs libraries needed (e.g. phaser)
+
+## Running Locally
+
+From the project directy run `npm run start:dev`  this creates a web server running on localhost:9997 and launches
+the default page. 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 
 
+import 'phaser';
+
+import BoardScene from  './scenes/BoardScene';
+
+const gameConfig = {
+  parent: "root",
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: BoardScene
+};
+
+
+
+
 function initializeSystem() {
-  rootDiv = document.getElementById('root');
-  greeting = document.createTextNode("Hello World Dynamic");
-  rootDiv.appendChild(greeting)
+  const rootDiv = document.getElementById('root');
+ // greeting = document.createTextNode("Hello World Dynamic");
+ // rootDiv.appendChild(greeting)
+  new Phaser.Game(gameConfig);
 }
 
 document.addEventListener('DOMContentLoaded', initializeSystem);
